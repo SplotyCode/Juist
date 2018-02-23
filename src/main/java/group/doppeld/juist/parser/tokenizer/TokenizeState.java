5c2b74692b63f109960635db7de927642de9ff83
adsad adsads <davidscandurra@gmail.com>
2018-02-23 10:49:44 +0000

    Update TokenizeState.java

--- src/main/java/group/doppeld/juist/parser/tokenizer/TokenizeState.java
+++ src/main/java/group/doppeld/juist/parser/tokenizer/TokenizeState.java
@@ -1,6 +1,17 @@
 package group.doppeld.juist.parser.tokenizer;
 
 public abstract class TokenizeState {
-
+    
+    private boolean cancelOthers = false;
+    
     public abstract void handleChar(Tokenizer tokenizer);
+    
+    public boolean isCancelOthers(){
+        return cancelOthers;    
+    }
+    
+    public void setCancelOthers(boolean cancelOthers){
+        this.cancelOthers = cancelOthers;
+        
+    }
 }
