411b7a837075e9e65bae04de29770165b579803d
Dominik <birkeldominik@aol.com>
2018-03-02 09:53:28 +0000

    Update NullVariable.java

--- src/main/java/group/doppeld/juist/runbox/variable/NullVariable.java
+++ src/main/java/group/doppeld/juist/runbox/variable/NullVariable.java
@@ -3,7 +3,7 @@ package group.doppeld.juist.runbox.variable;
 import group.doppeld.juist.runbox.VariableType;
 
 
-public class LinkedVariable<T> extends Variable<T> {
+public class NullVariable<T> extends Variable<T> {
     
     public LinkedVariable(VariableType type) {
         super(type);
