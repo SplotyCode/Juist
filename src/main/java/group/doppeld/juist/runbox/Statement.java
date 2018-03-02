ef0e03e5d8293de253b07716314f9067525cfa07
Dominik <birkeldominik@aol.com>
2018-03-02 09:38:11 +0000

    Update Statement.java

--- src/main/java/group/doppeld/juist/runbox/Statement.java
+++ src/main/java/group/doppeld/juist/runbox/Statement.java
@@ -3,6 +3,6 @@ package group.doppeld.juist.runbox;
 //TODO
 public abstract class Statement {
 
-    public abstract void run(Script script, long currentCall);
+    public abstract boolean run(Script script, long currentCall);
 
 }
