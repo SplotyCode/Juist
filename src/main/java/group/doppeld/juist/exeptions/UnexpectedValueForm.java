d60c7c9e3db6fe44d27d3ed206a6abc44ce86930
dominik <birkeldominik@aol.com>
2018-03-07 23:19:43 +0100

    Neues Mathe Update für + rechnen beim erstellen von Variablen

---
+++ src/main/java/group/doppeld/juist/exeptions/UnexpectedValueForm.java
@@ -0,0 +1,21 @@
+package group.doppeld.juist.exeptions;
+
+import group.doppeld.juist.parser.tokenizer.Tokenizer;
+
+public class UnexpectedValueForm extends SyntaxException {
+
+    private final Tokenizer tokenizer;
+
+
+    public UnexpectedValueForm(Tokenizer tokenizer){
+        super("Values dont fit together" + tokenizer.getcChar() + "at Line" + tokenizer.getLine());
+            this.tokenizer = tokenizer;
+        }
+    }
+    public UnexpectedValueForm(Tokenizer tokenizer, String message){
+        super(message + "|" + "Values dont fit together" + tokenizer.getcChar() + "at Line" + tokenizer.getLine());
+            this.tokenizer = tokenizer;
+        }
+
+    }
+}
--- src/main/java/group/doppeld/juist/parser/tokenizer/readers/value/NumberReader.java
+++ src/main/java/group/doppeld/juist/parser/tokenizer/readers/value/NumberReader.java
@@ -1,6 +1,7 @@
 package group.doppeld.juist.parser.tokenizer.readers.value;
 
 import group.doppeld.juist.exeptions.UnexpectedCharException;
+import group.doppeld.juist.exeptions.UnexpectedValueForm;
 import group.doppeld.juist.parser.tokenizer.TokenizeReader;
 import group.doppeld.juist.parser.tokenizer.Tokenizer;
 import group.doppeld.juist.parser.tokenizer.tokens.VariableValueToken;
@@ -30,37 +31,77 @@ public class NumberReader extends TokenizeReader {
         }
         return hasFloatingPoint?VariableValueToken.VariableType.DOUBLE: VariableValueToken.VariableType.INTEGER;
     }
-
+    private enum PrivateStates{
+        BEGINNING,
+        CHECKPLUS
+    }
+    private PrivateStates privateStates = PrivateStates.BEGINNING;
     private String content = "";
+    private String contentOfValueTwo = "";
     private boolean hasFloatingPoint = false;
 
     @Override
     public void handleChar(Tokenizer tokenizer) throws UnexpectedCharException {
+
         if(isCancelOthers()){
+            switch (privateStates){
+                case BEGINNING:
             if(ListUtil.containsArray(tokenizer.getcChar(), inNumberChars)) {
                 content += tokenizer.getcChar();
+
                 if(tokenizer.getcChar() == '.') {
                     if(hasFloatingPoint) throw new UnexpectedCharException(tokenizer, "What do you want to do with that point we already have one???");
                     else hasFloatingPoint = true;
+                    }
+                if(tokenizer.getcChar() =='+') {
+                    privateStates = PrivateStates.CHECKPLUS;
                 }
                 if(tokenizer.next() == ';'){
                     setCancelOthers(false);
+
                     close(tokenizer, new VariableValueToken(getVariableTypebyNumber(content, Character.MIN_VALUE), content));
                     content = "";
                 }
             } else if(ListUtil.containsArray(tokenizer.getcChar(), endChars)) {
                 setCancelOthers(false);
+                char firstCharEnd = tokenizer.getcChar();
                 close(tokenizer, new VariableValueToken(getVariableTypebyNumber(content, tokenizer.getcChar()), content));
                 content = "";
+
             }else{
-                setCancelOthers(false);
+            setCancelOthers(false);
                 close(tokenizer, new VariableValueToken(getVariableTypebyNumber(content, Character.MIN_VALUE), content));
                 content = "";
+            }case CHECKPLUS:
+                setIgnoreWhitespace(true);
+                        tokenizer.next();
+                        contentOfValueTwo += tokenizer.getcChar();
+                        String Lösung = content += contentOfValueTwo;
+                        setIgnoreWhitespace(false);
+
+                     if(tokenizer.next() == ';') {
+
+                         if (tokenizer.getcChar() == firstCharEnd) {
+
+
+
+                         close(tokenizer, new VariableValueToken(getVariableTypebyNumber(Lösung, firstCharEnd), Lösung));
+                         }else{
+                             throw new UnexpectedValueForm(tokenizer, "This Values don´t fit together");
+                         }
+                        break;
+                    }
+                    if(contentOfValueTwo == "0")
+                        break;
+
+
             }
+
         }else if(ListUtil.containsArray(tokenizer.getcChar(), startChars)){
             hasFloatingPoint = false;
             setCancelOthers(true);
             content += tokenizer.getcChar();
         }
-    }
-}
+    }   }
+
+
